01d5a62cfedc0af3b895d5be3698f4a2caa006e2
Maxi <maxi.britez@redb.ee>
2018-08-28 21:44:33 -0300

    Initial Commit

---
+++ src/main/java/Example.java
@@ -0,0 +1,6 @@
+public class Example {
+
+  public Boolean test() {
+    return true;
+  }
+}
---
+++ src/test/java/ExampleTests.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTests {
+
+  @Test
+  public void test() {
+    Example example = new Example();
+    Assert.assertTrue(example.test());
+  }
+}
